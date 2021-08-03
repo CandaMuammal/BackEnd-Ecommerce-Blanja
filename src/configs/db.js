9ce6c36814994ee090ff84d4b26c225cd49fee7e
CandaMuammal <scmuammal@gmail.com>
2021-08-03 16:53:52 +0700

    add db

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD
 })
 
 module.exports = connection

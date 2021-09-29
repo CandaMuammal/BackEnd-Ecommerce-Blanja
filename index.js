02781ce45550f701ee23d0012943e3679e1fd402
CandaMuammal <scmuammal@gmail.com>
2021-09-29 09:09:33 +0700

    deploy

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors())
 
 app.use(setCors())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE') // If needed
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type') // If needed
   res.setHeader('Access-Control-Allow-Credentials', true) // If needed

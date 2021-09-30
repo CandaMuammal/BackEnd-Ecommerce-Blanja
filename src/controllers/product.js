5ade93af2d7d3c991e8ba8d8ba3d391065680e19
CandaMuammal <scmuammal@gmail.com>
2021-10-01 04:00:54 +0700

    upload img cloud

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,9 +1,15 @@
 const productModel = require('../models/product')
 const helpers = require('../helpers/helper')
 const createError = require('http-errors')
+const cloudinary = require('cloudinary').v2;
+const { configCloudinary } = require('../middlewares/cloudinary');
+const path = require("path");
+const { images } = require('../middlewares/multer')
 // const fs = require('fs')
 // const redis = require('redis')
 // const client = redis.createClient(6379)
+
+cloudinary.config(configCloudinary);
 
 const getAllProduct = (req, res, next) => {
   const page = req.query.page || 1
@@ -13,12 +19,12 @@ const getAllProduct = (req, res, next) => {
 
   // console.log(start)
   // console.log(limit)
-  
+
   productModel.getAllProduct(start, limit, search)
     .then((result) => {
       const product = result
       // console.log(product)
-      
+
       // client.setex('allProduct', 60 * 60, JSON.stringify(product))
       helpers.responseGet(res, product, 200, null, page)
     })
@@ -59,8 +65,17 @@ const getProductById = (req, res, next) => {
     })
 }
 
-const insertProduct = (req, res, next) => {
+const insertProduct = async (req, res, next) => {
+
   const { name, price, color, size, category, idCategory, image, stock, description } = req.body
+  console.log(req.file)
+  const fileUpload = req.file;
+
+  const images = [];
+  const { path } = fileUpload;
+  images.push(path);
+
+  const toStr = await images.toString();
   const data = {
     name: name,
     price: price,
@@ -68,14 +83,11 @@ const insertProduct = (req, res, next) => {
     size,
     category,
     idCategory: 1,
-    image: `http://localhost:4000/file/${req.file.filename}`,
+    image: toStr,
     stock: stock,
     description,
     createdAt: new Date()
   }
-  console.log(req.file.filename)
-
-  // fs.unlinkSync(`./images/${req.file.filename}`)
 
   productModel.insertProduct(data)
     .then((result) => {
@@ -88,21 +100,73 @@ const insertProduct = (req, res, next) => {
     })
 }
 
-const updateProduct = (req, res) => {
+// const { name, price, color, size, category, idCategory, image, stock, description } = req.body
+// const data = {
+//   // name: name,
+//   // price: price,
+//   // color,
+//   // size,
+//   // category,
+//   // idCategory: 1,
+//   image: `http://localhost:4000/file/${req.file.filename}`,
+//   // stock: stock,
+//   // description,
+//   createdAt: new Date()
+// }
+// console.log("filename", req.file.filename)
+// console.log(data.image)
+
+
+// // fs.unlinkSync(`./images/${req.file.filename}`)
+
+//   productModel.insertProduct(data)
+//     .then((result) => {
+//       const product = result
+//       helpers.responseInsert(res, product, 200, null)
+//     })
+//     .catch((error) => {
+//       const err = new createError.InternalServerError()
+//       next(err)
+//     })
+// }
+
+const updateProduct = async (req, res) => {
   const id = req.params.id
-  const { name, price, color, size, idCategory, image, stock, description, category } = req.body
+  const { name, price, color, size, category, idCategory, image, stock, description } = req.body
+  console.log(req.file)
+  const fileUpload = req.file;
+
+  const images = [];
+  const { path } = fileUpload;
+  images.push(path);
+
+  const toStr = await images.toString();
   const data = {
     name: name,
     price: price,
-    color: color,
-    size: size,
-    idCategory: idCategory,
+    color,
+    size,
     category,
-    image: `http://localhost:4000/file/${req.file.filename}`,
+    idCategory: 1,
+    image: toStr,
     stock: stock,
     description,
     createdAt: new Date()
   }
+
+  // const { name, price, color, size, idCategory, image, stock, description, category } = req.body
+  // const data = {
+  //   name: name,
+  //   price: price,
+  //   color: color,
+  //   size: size,
+  //   idCategory: idCategory,
+  //   category,
+  //   image: `http://localhost:4000/file/${req.file.filename}`,
+  //   stock: stock,
+  //   description,
+  //   createdAt: new Date()
+  // }
   productModel.updateProduct(id, data)
     .then((result) => {
       const product = result
--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,13 +1,26 @@
 const multer = require('multer')
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './images')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const { configCloudinary } = require('./cloudinary');
 
-})
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './images')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname)
+//   }
+
+// })
+
+cloudinary.config(configCloudinary);
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'Blanja',
+  },
+});
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
@@ -27,3 +40,4 @@ const images = multer({
 })
 
 module.exports = images
+// module.exports = { uploadFile };
